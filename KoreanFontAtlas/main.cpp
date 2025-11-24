d8f89eeacb38a4e614dd51cdcc1f2d520f41b9e2
intelli21c <intelli21c@physics.re.kr>
2025-11-24 21:14:07 +0900

    Initial commit

---
+++ KoreanFontAtlas/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#pragma comment(lib, "freetype.lib")
+
+
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
